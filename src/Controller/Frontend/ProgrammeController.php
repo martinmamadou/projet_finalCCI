4647f50c249198742a47dc5a6bcb280074fab3bb
Mamad <mohamrtn2003@gmail.com>
2024-04-22 16:33:26 +0200

    feat(programme): css display index page v1

--- src/Controller/Frontend/ProgrammeController.php
+++ src/Controller/Frontend/ProgrammeController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller\Frontend;
 
+use App\Repository\ProgrammeMaisonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/programmes', 'user.programmes')]
 class ProgrammeController extends AbstractController
 {
-    #[Route('/frontend/programme', name: 'app_frontend_programme')]
+    public function __construct(
+        private readonly ProgrammeMaisonRepository $proMaisonRepo
+    ) {
+    }
+
+    #[Route('', name: '.index', methods: ['GET', 'POST'])]
     public function index(): Response
     {
-        return $this->render('frontend/programme/index.html.twig', [
-            'controller_name' => 'ProgrammeController',
+        return $this->render('Frontend/Programme/index.html.twig', [
+            'programmes' => $this->proMaisonRepo->findAll()
         ]);
     }
 }
